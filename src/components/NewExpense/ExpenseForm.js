a090d92f9112920d81dc25d03978090888e2802f
atmygml <atmygml@gmail.com>
2022-04-19 14:19:38 +0800

    ..

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -31,6 +31,9 @@ const ExpenseForm = () => {
 		};
 
 		console.log(expenseData);
+		setEnteredTitle('');
+		setEnteredAmount('');
+		setEnteredDate('');
 	};
 
 	return (
@@ -38,7 +41,11 @@ const ExpenseForm = () => {
 			<div className='new-expense__controls'>
 				<div className='new-expense__control'>
 					<label>Title</label>
-					<input type='text' onChange={titleChangeHandler} />
+					<input
+						type='text'
+						value={EnteredTitle}
+						onChange={titleChangeHandler}
+					/>
 				</div>
 				<div className='new-expense__control'>
 					<label>Amount</label>
@@ -46,6 +53,7 @@ const ExpenseForm = () => {
 						type='number'
 						min='0.01'
 						step='0.01'
+						value={EnteredAmount}
 						onChange={amountChangeHandler}
 					/>
 				</div>
@@ -55,6 +63,7 @@ const ExpenseForm = () => {
 						type='date'
 						min='2019-01-01'
 						max='2022-12-31'
+						value={EnteredDate}
 						onChange={dateChangeHandler}
 					/>
 				</div>

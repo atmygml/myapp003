6a21384b75872a4d6ceda53b7e6c8077682a7b21
atmygml <atmygml@gmail.com>
2022-04-19 12:34:48 +0800

    ..

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -13,7 +13,7 @@ const ExpenseForm = () => {
 					<label>Amount</label>
 					<input type='number' min='0.01' step='0.01' />
 				</div>
-				<div className='new-expense_control'>
+				<div className='new-expense__control'>
 					<label>Date</label>
 					<input type='date' min='2019-01-01' max='2022-12-31' />
 				</div>

02bba9ff3de2e9723d99dc4cb4077e57048736a3
atmygml <atmygml@gmail.com>
2022-04-22 12:16:17 +0800

    ...

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,6 +2,7 @@ import './ExpenseItem.css';
 import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const ExpenseItem = (props) => {
 	const [title, setTitle] = useState(props.title);
@@ -11,16 +12,20 @@ const ExpenseItem = (props) => {
 	};
 
 	return (
-		<Card className='expense-item'>
+		<div>
+			<ExpensesFilter />
 			<br />
-			<ExpenseDate date={props.date} />
-			<div className='expense-item__description'>
-				<h2>{title}</h2>
-				<div className='expense-item__price'>${props.amount}</div>
-			</div>
-			<button onClick={clickHandler}>Change Title</button>
-			<br />
-		</Card>
+			<Card className='expense-item'>
+				<br />
+				<ExpenseDate date={props.date} />
+				<div className='expense-item__description'>
+					<h2>{title}</h2>
+					<div className='expense-item__price'>${props.amount}</div>
+				</div>
+				<button onClick={clickHandler}>Change Title</button>
+				<br />
+			</Card>
+		</div>
 	);
 };
 

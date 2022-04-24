04077dcb2f45beaad487ffa2b7cab4f400042549
atmygml <atmygml@gmail.com>
2022-04-24 22:43:23 +0800

    ...

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,21 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-	const [title, setTitle] = useState(props.title);
+	//const [title, setTitle] = useState(props.title);
 
-	const clickHandler = () => {
+	/* const clickHandler = () => {
 		setTitle('Updated!!!...');
-	};
+	}; */
 
 	return (
 		<Card className='expense-item'>
 			<br />
 			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
 			</div>
-			<button onClick={clickHandler}>Change Title</button>
+			{/* <button onClick={clickHandler}>Change Title</button> */}
 			<br />
 		</Card>
 	);
